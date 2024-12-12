f0d99cd8f4ff1bbeea762c21b88c2883a85b081f
hyun-young <77002884+hyun-young@users.noreply.github.com>
2024-12-12 11:09:59 +0900

    [ADD/Pass] BOJ 1072

--- week01/4_BOJ_1072_게임/BOJ_1072_장현영.py
+++ week01/4_BOJ_1072_게임/BOJ_1072_장현영.py
@@ -10,7 +10,7 @@ input = sys.stdin.readline
 
 x,y = map(int, input().split())
 # 정수값이 바뀔 경우를 계산하자
-win_percent = int(y/x * 100)
+win_percent = int(y*100/x)
 if win_percent >= 99:
     print(-1)
     exit(0)
@@ -38,14 +38,31 @@ if win_percent >= 99:
 #         break
 
 # 100 단위 계산
-idx = -1
-for temp in range(0,int(1e9),100):
-    new_percent = (y+temp)/(x+temp) * 100
-    if win_percent+1 <= new_percent:
-        idx = temp
-        break
-for ans in range(idx-100, idx+1):
-    new_percent = (y + ans) / (x + ans) * 100
-    if win_percent+1 <= new_percent:
-        print(ans)
-        break
+# idx = -1
+# for temp in range(0,int(1e9),100):
+#     new_percent = (y+temp)/(x+temp) * 100
+#     if win_percent+1 <= new_percent:
+#         idx = temp
+#         break
+# for ans in range(idx-100, idx+1):
+#     new_percent = (y + ans) / (x + ans) * 100
+#     if win_percent+1 <= new_percent:
+#         print(ans)
+#         break
+
+# 범위 전체로 해서 이분 탐색
+start, end = 1, 1000000000
+while start < end:
+    mid = (start + end) // 2
+
+    # x, y에 mid를 각각 더해서 퍼센트를 구하고,
+    # 처음 x, y의 퍼센트와 비교한다.
+    cur = int((y+mid)*100/(x+mid))
+    if cur <= win_percent:
+        start = mid+1
+    else:
+        end = mid
+if int((y+end)/(x+end)*100) > win_percent:
+    print(end)
+else:
+    print(-1)
