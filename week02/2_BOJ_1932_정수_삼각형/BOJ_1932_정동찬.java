ffa49ca94beec698a385a6e6f57b84b40469ff4a
unknown <iiky2124@gmail.com>
2024-12-15 20:35:35 +0900

    update readme

--- week02/2_BOJ_1932_정수_삼각형/BOJ_1932_정동찬.java
+++ week02/2_BOJ_1932_정수_삼각형/BOJ_1932_정동찬.java
@@ -3,6 +3,7 @@ package Q1932;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Q1932 {
@@ -24,20 +25,27 @@ class Q1932 {
 		for (int lr = 1; lr < N; lr++) {
 			String[] line = br.readLine().split(" ");
 			
+			// 삼각형에 값 넣기
+			// line의 값에, 부모 요소의 값을 추가할 것
 			for (int lc = 0; lc < lr + 1; lc++) {
+				// 맨 좌측이면 자신 값에 부모 요소 값을 추가
 				if (lc == 0) {
 					T[lr][lc] = T[lr - 1][lc] + Integer.parseInt(line[lc]);
 					continue;
 				}
+				
+				// 맨 우측이면 자신 값에 부모 요소 값을 추가
 				if (lc == lr) {
 					T[lr][lc] = T[lr - 1][lc - 1] + Integer.parseInt(line[lc]);
 					continue;
 				}
 				
+				// 자신 값에 부모 요소 둘 중 큰 값을 추가
 				T[lr][lc] = Integer.parseInt(line[lc]) + Math.max(T[lr - 1][lc - 1], T[lr - 1][lc]);
 			}
 		}
 
+		// 맨 마지막 줄의 가장 큰 값을 출력
 		System.out.println(Arrays.stream(T[N - 1]).max().getAsInt());
 	}
 
--- week02/3_BOJ_1629_곱셈/BOJ_1629_정동찬.java
+++ week02/3_BOJ_1629_곱셈/BOJ_1629_정동찬.java
@@ -5,14 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Q1629 {
+	// 나머지 분배 법칙
+	// (a x b) % c = ((a % c) * (b % c)) % c
+	
 	public static long divide(long a, long b, long c) {
+		// 가장 잘게 쪼개고 나면 b == 0 or 1
+		// b == 0이면 1 % c = 1을 반환
 		if (b == 0) return 1;
 
+		// b == 1이면 half = a % c
 		long half = divide(a, b / 2, c);
+
 		long result = (half * half) % c;
 		
+		// b가 홀수라면 b = 2 * (b // 2) + 1
+		// + 1 부분을 추가로 처리해야 함
 		if (b % 2 != 0) {
-			result = (result * a) % c;
+			result = (result * (a % c)) % c;
 		}
 		
 		return result;
